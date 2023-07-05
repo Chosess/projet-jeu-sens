fab87e0c78ef7ce5761492f34ea2fc96182b594b
n3rd-asc <n3rd.asc@gmail.com>
2023-07-05 12:59:08 +0200

    Grid Fix + Colors

--- javascript/script.js
+++
@@ -1,7 +0,0 @@
- let test = document.querySelectorAll(".test>div");
-
- for(let x = 1; x < 14; x++){
-    test[x-1].style.position = "absolute";
-    test[x-1].style.left = ((x * 10) - 20) + "%";
-    test[x-1].style.transform = "rotate(" + -(x-7) * 10 + "deg)";
- }
